eddadb3603ff2e2044042bc9de0bf723818fd7f3
Laise de Moraes <laise.moraes@fiocruz.br>
2023-01-17 23:31:48 -0300

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -14,7 +14,7 @@ fi
 
 rm -rf $HOME/pvm-igm.github.io/REDCap_DIAGCOVID19_DATA_*.csv
 
-DATA="token=431A5D970FFD7855F1A5542CC1662EA5&content=report&format=csv&report_id=14363&rawOrLabel=raw&rawOrLabelHeaders=raw&exportCheckboxLabel=false&returnFormat=csv"
+DATA="token=${3}&content=report&format=csv&report_id=${4}&rawOrLabel=raw&rawOrLabelHeaders=raw&exportCheckboxLabel=false&returnFormat=csv"
 CURL=`which curl`
 $CURL -s -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json" -X POST -d $DATA https://bdp.bahia.fiocruz.br/api/ > $HOME/pvm-igm.github.io/REDCap_DIAGCOVID19_DATA_$(date +'%Y-%m-%d').csv
 
@@ -22,13 +22,13 @@ source activate flexdashboard
 
 cd $HOME/pvm-igm.github.io
 
-Rscript --vanilla -e "rmarkdown::render('index.Rmd')" $HOME/pvm-igm.github.io
+Rscript --vanilla -e "rmarkdown::render('index.Rmd')" ${1} ${2} ${5} ${6} ${7} ${8}
 
 git add "index.html"
 git status
-git config user.name "Laise de Moraes"
-git config user.email "laise.moraes@fiocruz.br"
+git config user.name ${9}
+git config user.email ${10}
 git commit -m "PVM Dashboard $(date +'%Y-%m-%d')"
 git remote remove origin
-git remote add origin https://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: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@gitlab.com/pvm-igm/pvm-igm.github.io.git
-git push https://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@github.com/pvm-igm/pvm-igm.github.io.git main
+git remote add origin https://${11}:${11}@gitlab.com/pvm-igm/pvm-igm.github.io.git
+git push https://${11}@github.com/pvm-igm/pvm-igm.github.io.git main
